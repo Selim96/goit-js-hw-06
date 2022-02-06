4829ff97a48b255d86f52c573507816ac6f0071b
Selim Fazylov <selim.fazylov1@gmail.com>
2022-02-07 01:55:27 +0200

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,12 @@
+const loginForm = document.querySelector('#login-form');
 
+
+const  handlClick = (event) => {
+  event.preventDefault();
+  const {
+    elements: { email, password }
+  } = event.currentTarget;
+  console.log(email.value, password.value);
+};
+
+loginForm.addEventListener("submit", handlClick);
