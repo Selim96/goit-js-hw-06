f000fe5b6c093e38ec4aa7ccb8c3d3906fbfec88
Selim Fazylov <selim.fazylov1@gmail.com>
2022-02-07 21:45:34 +0200

    task 8 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,12 +1,20 @@
-const loginForm = document.querySelector('#login-form');
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', handlClick);
 
-const  handlClick = (event) => {
+function handlClick(event) {
   event.preventDefault();
   const {
     elements: { email, password }
   } = event.currentTarget;
-  console.log(email.value, password.value);
-};
 
-loginForm.addEventListener("submit", handlClick);
+  if (email.value === "" || password.value === "") {
+    return alert("Please fill in all the fields!");
+  };
+  const loginData = {
+    email: email.value,
+    password: password.value,
+  };
+  console.log(loginData);
+  event.currentTarget.reset();
+};
