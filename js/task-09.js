34cb90a3f7d9f45ce3ae5da9e2381fe12fbf52c6
Selim Fazylov <selim.fazylov1@gmail.com>
2022-02-07 22:21:11 +0200

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
+const body = document.body;
+const btn = document.querySelector('.change-color')
+const color = document.querySelector('.color');
+
+btn.addEventListener('click', handalclick);
+
+function handalclick() {
+  body.style.backgroundColor = getRandomHexColor();
+  color.textContent = `${getRandomHexColor()}`;
+};
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
